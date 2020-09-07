2c94d9a354c3482fe59f5ac53e8d7aa9ee753d40
Zita Nemeckova <znemecko@redhat.com>
2020-09-07 13:09:58 +0200

    Remove unused route

--- src/loaders/standalone/routes.tsx
+++ src/loaders/standalone/routes.tsx
@@ -81,7 +81,6 @@ export class Routes extends React.Component<{}> {
   routes = [
     { comp: GroupList, path: Paths.groupList },
     { comp: GroupDetail, path: Paths.groupDetail },
-    //{ comp: GroupCreate, path: Paths.createGroup },
     { comp: UserProfile, path: Paths.userProfileSettings },
     { comp: UserCreate, path: Paths.createUser },
     { comp: EditUser, path: Paths.editUser },
